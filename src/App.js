83f67a87956338a130fd9938ec1deb706a66616d
mustafaaydin <aydinmustafa9817@gmail.com>
2023-05-09 14:52:15 +0300

    firebase auth entegre edildi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect }from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
@@ -7,32 +7,17 @@ import PrivateRoute from "./PrivateRoute";
 import "./App.css";
 import Header from "./pages/Header";
 
-
-import { useDispatch, useSelector } from 'react-redux';
-import { login, logout } from './features/userSlice';
-import {onAuthStateChanged} from 'firebase/auth';
-import {auth} from './firebase';
-import { selectUser } from './features/userSlice';
-
 function App() {
-  const user = useSelector(selectUser);
-  const dispatch = useDispatch();
- 
-  
-
-
   return (
-    
-      <Router>
-        <Switch>
-        <Route path="/auth" component={Auth} /> 
+    <Router>
+      <Switch>
+        <Route path="/auth" component={Auth} />
 
-          <PrivateRoute path="/" exact component={Home} />
-          <PrivateRoute path="/books" component={Header} />
-          <PrivateRoute path="/likes" component={Home} />
-        </Switch>
-      </Router>
-    
+        <PrivateRoute path="/" exact component={Home} />
+        <PrivateRoute path="/books" component={Header} />
+        <PrivateRoute path="/likes" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
